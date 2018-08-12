aeec79efb5a83bd20e1d2848352e3b46c3fb6220
Amrit Agrahari <amritagrahari@gmail.com>
2018-08-12 12:46:57 +0530

    ye wala please dekh lo

--- test.py
+++ test.py
@@ -1,12 +1,23 @@
-# will be developed asap
-# dev mein hain fir se
+# version 2.0
 
-def starPattern():
-	for i in range (5):
+# method to generate pattern of length 'k'
+def starPattern(k):
+	for i in range (k):
 		for j in range(i+1):
 			print "*",
 		print ""	
 	
+# ask for user input for 'k'
+print "Please enter any integer - ", 
+k = input ()
 
-starPattern()
+# print patter if k is an integer, else print "ERROR"
+if (isinstance(k, (int, long))):
+	starPattern(k)
+
+else:
+	print "ERROR"
+
+
+
 
